6ced0f9f4bbdc168aba3264dac45053f72978371
Matias <carrillomatias121@gmail.com>
2026-05-06 21:25:25 +0000

    refactor: simplify cliente perfil view

--- app/views/cliente/perfilView.php
+++ app/views/cliente/perfilView.php
@@ -9,8 +9,7 @@
             margin-top:20px;
         }
     </style>
-
-
+    
     <title>Perfil</title>
 </head>
 <body>
@@ -18,33 +17,5 @@
     <form action="/proyecto-SO/public/index.php/perfil/carrito" method="GET">
         <button type="submit">Ver Carrito</button>
     </form>
-
-    <h2>Agregar Producto</h2>
-    <form action="/proyecto-SO/public/index.php/perfil/agregar" method="POST">
-        <input type="number" name="id_producto" placeholder="Ingrese ID producto" required>
-        <input type="number" name="cantidad" placeholder="Ingrese cantidad" required>
-        <button type="submit">Agregar</button>
-    </form>
-
-    <h2>Eliminar Producto</h2>
-    <form action="/proyecto-SO/public/index.php/perfil/eliminar" method="POST">
-        <input type="number" name="id_producto" placeholder="Ingrese ID producto" required>
-        <button type="submit">eliminar</button>
-    </form>
-
-    <form action="proyecto-SO/public/index.php">
-        <button type="submit" class="btn-cerrar-sesion">Cerrar Sesión</button>
-    </form>
-
-    <?php if(isset($_SESSION['agregar'])): ?>
-        <?php if($_SESSION['agregar'] === TRUE): ?>
-            <p>Producto agregado correctamente</p>
-        <?php else: ?>
-            <p>El producto no existe</p>
-        <?php endif ?>
-    <?php endif ?>
-    <?php unset($_SESSION['agregar']); ?>
-
-
 </body>
 </html>
