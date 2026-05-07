928cc608743b553973da26bc16f8c92c62b924dc
Matias <carrillomatias121@gmail.com>
2026-05-07 05:38:44 +0000

    feat: add styles and html to homeView

--- app/views/home/homeView.php
+++ app/views/home/homeView.php
@@ -3,18 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <style>
-        button {
-            margin-top:20px;
-        }
-    </style>
-
-
-    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/home.css">
+    <title>Electro Smart</title>
 </head>
 <body>
-    <h1>ElectroSmart</h1>
-    
+    <header>  
+        <a href="#">
+            <span>Electro Smart</span>
+        </a>
+        <nav>
+            <ul>
+                <li><a href="#">Catálogo</a></li>
+                <li><a href="#">Destacados</a></li>
+            </ul>
+        </nav>
+        <div>
+            <button>
+                <i class="fa-solid fa-user"></i>
+                Iniciar Sesión
+            </button>
+        </div>
+    </header>
+
+    <main>
+        <section class="hero">
+            <div class="hero-content">
+                <span class="etiqueta">
+                    <i class="fa-solid fa-bolt"></i>
+                    Nueva colección 2026
+                </span>
+                <h1>Tecnología que <span class="red-text">se <br>siente.</span> </h1>
+                <p>Descubre dispositivos pensados para integrarse perfectamente en tu día a día.</p>
+                <div>
+                    <button class="explorar-catalogo-btn">
+                        Explorar catálogo
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                </div>
+                <div class="rating">
+                    <div class="rating-estrellas">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+                    4.9 / 12k reseñas
+                </div>
+            </div>
+
+            <div class="hero-img">
+                <img src="assets/img/img-hero.jpg" alt="img hero">
+            </div>
+        </section>
+    </main>
+
+
 </body>
 </html>
