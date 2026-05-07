631123cd0c55c31f44eb0bba4cb1e53475dd0be8
Matias <carrillomatias121@gmail.com>
2026-05-07 00:35:01 +0000

    feat: add webRouter

---
+++ app/routes/webRouter.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__ . '/../controllers/homeController.php';
+
+class WebRouter {
+
+    private $routes = [
+        '/' => [HomeController::class, 'index']
+    ];
+
+    public function route($url,$conn) {
+
+        if(!isset($this->routes[$url])) {
+            http_response_code(404);
+            echo "Página no encontrada";
+            return;
+        }
+
+        [$genericController, $method] = $this->routes[$url];
+
+        $controller = new $genericController($conn);
+        $controller->$method();
+
+    }
+
+    
+}
+
+
+?>
