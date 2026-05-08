91783e19d56377df96823a799cf2b2820138883a
Matias <carrillomatias121@gmail.com>
2026-05-08 20:49:15 +0000

    add login routing

--- app/routes/webRouter.php
+++ app/routes/webRouter.php
@@ -1,10 +1,12 @@
 <?php
 require_once __DIR__ . '/../controllers/homeController.php';
+require_once __DIR__ . '/../controllers/authController.php';
 
 class WebRouter {
 
     private $routes = [
-        '/' => [HomeController::class, 'index']
+        '/' => [HomeController::class, 'index'],
+        '/auth/login' => [AuthController::class,'index']
     ];
 
     public function route($url,$conn) {
