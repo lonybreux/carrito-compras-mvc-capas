4c6f417db257b9d7fc7dbfc6066af47cdfe85190
Matias <carrillomatias121@gmail.com>
2026-05-09 03:47:14 +0000

    feat: add catalogo routing

--- app/routes/webRouter.php
+++ app/routes/webRouter.php
@@ -1,13 +1,15 @@
 <?php
 require_once __DIR__ . '/../controllers/homeController.php';
 require_once __DIR__ . '/../controllers/authController.php';
+require_once __DIR__ . '/../controllers/catalogoController.php';
 
 class WebRouter {
 
     private $routes = [
         '/' => [HomeController::class, 'index'],
         '/auth/login' => [AuthController::class,'index'],
-        '/auth/register' =>[AuthController::class,'registrar']
+        '/auth/register' => [AuthController::class,'registrar'],
+        '/catalogo' => [CatalogoController::class,'index']
     ];
 
     public function route($url,$conn) {
