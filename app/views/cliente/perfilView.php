4d3a50ac4f2fe7f20b6991c7222b4eec6930623a
Matias <carrillomatias121@gmail.com>
2026-05-10 17:25:45 +0000

    remove unused destacados section

--- app/views/cliente/perfilView.php
+++ app/views/cliente/perfilView.php
@@ -21,7 +21,6 @@
         <nav>
             <ul>
                 <li><a href="/proyecto-SO/public/index.php/catalogo">Catálogo</a></li>
-                <li><a href="/proyecto-SO/public/index.php/destacados">Destacados</a></li>
             </ul>
         </nav>
         <div> 
