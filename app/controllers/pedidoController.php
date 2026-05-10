02526d22c20667e6c9e5370a3e4617d21bda49f4
Matias <carrillomatias121@gmail.com>
2026-05-10 23:15:25 +0000

    feat: add pedido controller

--- app/controllers/pedidoController.php
+++ app/controllers/pedidoController.php
@@ -1,10 +1,13 @@
 <?php
+require_once __DIR__ . '/../services/carritoService.php';
 
 class PedidoController {
     private $conn;
+    private $carritoService;
 
     public function __construct($conn){
         $this->conn = $conn;
+        $this->carritoService = new CarritoService($conn);
     }
 
     public function registrarPedido() {
@@ -12,6 +15,14 @@ class PedidoController {
             
             $direccion = $_POST['direccion'];
             $metodoPago = $_POST['metodo_pago'];
+            $id_cliente = $_SESSION['id_cliente'];
+            $carrito = $this->carritoService->obtenerGuardarCarrito($id_cliente);
+            $total = $this->carritoService->obtenerTotal($carrito->getId());
+            $id_pedido = $this->carritoService->obtenerPedido($id_cliente);
+
+            $this->carritoService->registrarPedido($total,$id_cliente);
+            $this->carritoService->registrarPago($total,$metodoPago,$id_pedido);
+            $this->carritoService->registrarEnvio($direccion,$id_pedido);
 
             header('Location: proyecto-SO/public/index.php/pedido/exito');
             exit();
